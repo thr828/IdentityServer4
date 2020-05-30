720a1e407e026c1e4d533777895d1aa85ae93d54
thr828 <592040808@qq.com>
2020-05-30 15:22:40 +0800

    重新嵌入代码

---
+++ PasswordCredentialConsole/Program.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+
+namespace IdentityServerConsole
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            // 从元数据中发现端口
+            // 调用API
+            var client = new HttpClient();
+            var disco = await client.GetDiscoveryDocumentAsync("http://localhost:5000");
+            // 请求以获得令牌
+     
+            var tokenResponse = await client.RequestPasswordTokenAsync(
+                new  PasswordTokenRequest()
+                {
+                    Address = disco.TokenEndpoint,
+                    ClientId = "pwdClient",
+                    ClientSecret = "secret",
+                    Scope = "api",
+                    UserName = "jesse",
+                    Password = "123456",
+                });
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+          
+            client.SetBearerToken(tokenResponse.AccessToken);
+
+            var response = await client.GetAsync("http://localhost:5002/WeatherForecast");
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.StatusCode);
+            }
+            else
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(JArray.Parse(content));
+            }
+
+        }
+    }
+}
